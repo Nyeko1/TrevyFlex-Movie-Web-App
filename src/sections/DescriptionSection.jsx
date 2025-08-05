6a1368e7bc1f7fd33db296ab7916f03cbe8932a1
Trevor <155629544+Nyeko1@users.noreply.github.com>
2025-08-05 11:31:14 +0300

    Update DescriptionSection.jsx
    adjusted the size of the pop-view-box on both large and small screens

--- src/sections/DescriptionSection.jsx
+++ src/sections/DescriptionSection.jsx
@@ -17,21 +17,33 @@ const DescriptionSection = ({ overview }) => {
         </button>
       </div>
 
-      {showModal && (
-        <div
-          ref={popupRef}
-          className="absolute left-10 mt-2 w-[300px]
-           max-h-[200px] border-4 border-[#cae4f9] bg-white
-            text-black p-4 rounded-lg shadow-lg z-50 text-left flex flex-col overflow-y-auto"
-        >
-          <p className="text-sm leading-relaxed">{overview}</p>
-          <button
+     {showModal && (
+        <>
+          {/* Modal Overlay */}
+          <div 
+            className="fixed inset-0  z-40"
             onClick={() => setShowModal(false)}
-            className="mt-3 text-blue-600 hover:underline text-xs mx-auto block"
+          />
+          
+          {/* Modal Content */}
+          <div
+            ref={popupRef}
+            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                     w-[85vw] max-w-[320px] max-h-[40vh]
+                     sm:absolute sm:top-0 sm:left-50 sm:right-0 sm:transform-none 
+                     sm:w-100 sm:max-w-none sm:max-h-[200px] sm:mx-2 sm:mt-2
+                     bg-white text-black p-4 rounded-lg shadow-lg z-50 
+                     border-4 border-[#cae4f9] overflow-y-auto"
           >
-            Show less
-          </button>
-        </div>
+            <p className="text-sm leading-relaxed">{overview}</p>
+            <button
+              onClick={() => setShowModal(false)}
+              className="mt-3 text-blue-600 hover:underline text-xs mx-auto block"
+            >
+              Show less
+            </button>
+          </div>
+        </>
       )}
     </>
   );
